7fdd7781d24d784523fa28ffdfb7bf16d1558d3b
komanosuke <toshihiko.komai616@gmail.com>
2025-04-18 00:10:04 +0900

    登録画面のスピナーレイヤーの修正

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 export default function LoadingSpinner() {
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
             <div className="animate-spin rounded-full h-8 w-8 border-4 border-gray-200 border-t-4 border-t-blue-500"></div>
         </div>
     );
